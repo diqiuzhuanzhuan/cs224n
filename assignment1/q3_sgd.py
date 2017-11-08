81d838103d58f51dd5108b2b3ffd4f6daf918e89
za-malong <malong@zhongan.io>
2017-11-08 21:31:01 +0800

    1. 实现了sgd含税

--- assignment1/q3_sgd.py
+++ assignment1/q3_sgd.py
@@ -85,7 +85,10 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
 
         cost = None
         ### YOUR CODE HERE
-        raise NotImplementedError
+#        raise NotImplementedError
+        cost, grad = f(x)
+        x -= step * grad
+
         ### END YOUR CODE
 
         if iter % PRINT_EVERY == 0:
@@ -132,7 +135,7 @@ def your_sanity_checks():
     """
     print "Running your sanity checks..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+    #raise NotImplementedError
     ### END YOUR CODE
 
 
